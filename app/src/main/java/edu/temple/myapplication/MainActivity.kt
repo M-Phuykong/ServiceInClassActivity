9f38fae2c3d0333836610a0899c87babcf721eda
Phuykong <m.phuykong@gmail.com>
2025-04-03 12:11:31 -0400

    add TimeHandler

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -5,18 +5,28 @@ import android.content.Intent
 import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var timeBinder: TimerService.TimerBinder
+    lateinit var timerTextView: TextView
     var isConnected = false
+
+    val timeHandler = Handler(Looper.getMainLooper()){
+        timerTextView.text = it.what.toString()
+        true
+    }
 
     val serviceConnect = object: ServiceConnection{
 
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
             timeBinder = service as TimerService.TimerBinder
+            timeBinder.setHandler(timeHandler)
             isConnected = true
         }
 
@@ -28,6 +38,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        timerTextView = findViewById<TextView>(R.id.textView)
 
         bindService(
             Intent(this, TimerService::class.java),
